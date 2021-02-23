279dfbaf310b533e9dead4203c8568d203fd3854
pandaking <aditya9660sharma@gmail.com>
2021-02-23 11:30:59 +0530

    File reading added and readme updated

---
+++ InputFile.py
@@ -0,0 +1,41 @@
+# file input as it is
+file = open('TestCase1.txt', 'r')
+
+# all lines in a list
+l = file.readlines()
+
+# extacting them in better way
+instructions = []
+
+# data simplification
+for list in l:
+    # removing starting and ending spaces
+    list = list.strip()
+
+    # enter when we have something in line
+    if len(list) > 0:
+        temp = ''
+        for k in list:
+            # removing comments
+            if k[0] == '#':
+                break
+            temp += k
+        # add better line
+        if len(temp) > 0:
+            instructions.append(temp)
+
+dataSegment = []
+
+i = 0
+# seprating data and instrution segment
+for inst in instructions:
+    i += 1
+    if inst == '.text':
+        break
+    dataSegment.append(inst)
+
+
+instructions = instructions[i-1:]
+
+# for inst in instructions:
+#     print(inst)
--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Bytify
 
-The project mainly focus on building a assembly language emulator with MIPS architecture.
+The project mainly focus on building a **Assembly language emulator** with **MIPS** architecture.
 
-# Function support in phase 1
+## Function support in phase 1
 
-1. Add
-2. SUB
-3. BNE
-4. Jump
-5. Ld (load)
-6. St (store)
+1. Add/SUB/MUL/DIV
+1. BEQ/BNE
+1. Jump
+1. Ld (load) / St (Store)
+1. AND/OR
 
-# Programming Language used
+### Programming Language used
 
-Python
+`Python`
