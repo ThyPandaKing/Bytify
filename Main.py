7cb57a57531b2dc1a11f66ce93bb237bf19fdf3a
pandaking <aditya9660sharma@gmail.com>
2021-02-24 21:31:36 +0530

    address dictionary added and Instructions ready to use

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ import InputFile as InputFile
 import re
 
 DataSegment = []
+Addres = {}
+
+
 Register = {
     '$R1': 0x0,
     'R2': 0x0,
@@ -20,10 +23,39 @@ def FillDataSegment(Data):
     pass
 
 
+def improveInstructions(Instructions):
+    pc = 0
+    temp = []
+    for inst in Instructions:
+
+        if '.' in inst:
+            temp.append(inst)
+        elif ':' in inst:
+            idx = inst.find(':')
+
+            Addres[inst[:idx]] = pc
+            if inst[-1] == ':':
+                temp.append(inst)
+            else:
+                idx = inst.find(':')
+                id2 = Instructions.index(inst)
+                Instructions[id2] = inst[idx+1:].strip()
+                pc += 1
+        else:
+            pc = pc+1
+
+    for tep in temp:
+        Instructions.remove(tep)
+
+
 MakeDataSegment()
+
 Instructions, Data = InputFile.InputFile()
 
-FillDataSegment(Data)
+FillDataSegment(Data)  # filling data segment
+improveInstructions(Instructions)  # for branch instructions
+
+
 PC = 0
 while 1:
     # instruction fetch -> increase pc , send inst to next guy
